6393db9d69149f7cf97b7a49cb99ebff24ab761f
Hafidh Bakari Juma <148118417+hafidhbakarjuma@users.noreply.github.com>
2024-06-15 00:04:31 +0200

    Add files via upload

---
+++ string test1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main ()
+{
+
+  char string1[] = "Demba";
+  char string2[] = "Code";
+
+
+   //strlwr (string1);                                      // converts a string to lowrcase
+  //strupr (string1);                                     // converts a string to uppercase
+  //strcat (string1, string2);                           // appends string2 to end of string1
+  //strncpy (string1, string2, 1);                       // appends n characters from string2 to string1
+  //strncpy (string1, string2);                        // copy string2 to string1
+  //strcpy (string1, string2, 4);                     // copy n characters of string2 to string1
+
+  //strset (string1, '?');                           // sets all characters of a string to a given character
+  //strnset (string1, 'x', 1);                      // sets first characters of string to a given character
+  // strrev (string1);                             // reverses a string
+
+  //int result = strlen(string1);                        //return string length as int
+  //nt result = strcmp(string1, string2);             // string compare all character
+  //int result = strncmp(string1, string2, 1);         // string compare n character
+  //int result = strcmpi(string1, string1);           // string compare all (ignore case)
+ //int result = strnicmp(string1, string1, 1);       // string compare n character (ignore case)
+
+
+  printf("%s ", string2);
+}
